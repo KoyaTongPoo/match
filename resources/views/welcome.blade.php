55c419e98601bff5509f68292cdb79ca88d31e69
KoyaTongPoo <blackcoffee001@gmail.com>
2020-03-11 19:54:56 +0900

    PWA周りを整備中

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,6 +4,15 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
+         <link rel="manifest" href="manifest.json" />
+ <script async src="https://cdn.jsdelivr.net/npm/pwacompat@2.0.8/pwacompat.min.js"
+     integrity="sha384-uONtBTCBzHKF84F6XvyC8S0gL8HTkAPeCyBNvfLfsqHh+Kd6s/kaS4BdmNQ5ktp1"
+     crossorigin="anonymous"></script>
+     
+
+
+
+
 
         <title>Laravel</title>
         <link rel="stylesheet" href="{{asset('css/app.css')}}">
